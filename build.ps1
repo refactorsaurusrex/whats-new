b7a49b0efe7d159990ab5ab47ae1a2e846e85eee
Nick Spreitzer <nick@refactorsaurusrex.com>
2018-10-10 16:46:33 -0500

    Regenerate module manifest at build time

--- build.ps1
+++ build.ps1
@@ -1,2 +1,45 @@
+param(
+  [string]$Version = ''
+)
+
 $ErrorActionPreference = 'Stop'
-choco pack .\whats-new\whats-new.nuspec --version $env:APPVEYOR_BUILD_VERSION
+
+if ($env:APPVEYOR_BUILD_VERSION) {
+  $Version = $env:APPVEYOR_BUILD_VERSION
+} elseif ($Version -eq '') {
+  throw "Missing version parameter"
+}
+
+$manifestPath = "$PSScriptRoot\src\whats-new.psd1"
+$guid = '861e5d28-8348-47d3-a2f6-cdd23e33bb55'
+$modules = Get-ChildItem -Path "$PSScriptRoot\src\modules" -File | 
+  Select-Object -ExpandProperty Name | 
+  ForEach-Object { ".\modules\$_" }
+
+$manifestArgs = @{
+  Path = $manifestPath
+  Guid = $guid
+  Author = 'Nick Spreitzer'
+  CompanyName = 'RAWR! Productions'
+  ModuleVersion = $Version
+  FunctionsToExport = @('*')
+  AliasesToExport = @('*')
+  NestedModules = $modules
+}
+
+New-ModuleManifest @manifestArgs
+$originalManifestContent = Get-Content $manifestPath
+$cleanManifest = @()
+
+$originalManifestContent | ForEach-Object { 
+  if ($_ -match "(?(?=.*#)(.*(?=#))|(.*))" -and -not [string]::IsNullOrWhiteSpace($Matches[0])) { 
+    $cleanManifest += $Matches[0]
+  } 
+}
+
+for ($i = 1; $i -le $cleanManifest.Length - 2; $i++) {
+  $cleanManifest[$i] = "`t$($cleanManifest[$i])"
+}
+
+Set-Content -Path $manifestPath -Value $cleanManifest
+choco pack .\src\whats-new.nuspec --version $Version
