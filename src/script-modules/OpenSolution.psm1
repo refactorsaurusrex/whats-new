a06e0be98840c6b4924d3097246f702f4aaec00e
Nick Spreitzer <nick@refactorsaurusrex.com>
2019-05-04 00:03:55 -0500

    General improvements to OpenSolution function

--- src/script-modules/OpenSolution.psm1
+++ src/script-modules/OpenSolution.psm1
@@ -8,19 +8,19 @@ The directory to search. Defaults to the current directory.
 function Open-Solution {
   [Alias('sln')]
   param (
-    [string]$RootDirectory = ''
+    [string]$RootDirectory = $PWD
   )
 
-  $solutions = Get-ChildItem -Recurse -Path "$RootDirectory*.sln"
+  $solutions = Get-ChildItem -Recurse -Path $RootDirectory -Filter "*.sln"
   if ($solutions.Count -eq 1) {
-    & $solutions.FullName
+    Invoke-Item $solutions.FullName
   }
   elseif ($solutions.Count -eq 0) {
-    write-host "I couldn't find any solution files here!"
+    Write-Host "I couldn't find any solution files here!" -ForegroundColor Red
   }
   elseif ($solutions.Count -gt 1) {
-    write-host "I found more than solution. Which one do you want to open?"
-    $solutions | ForEach-Object { write-host " - $($_.FullName)" }
+    Write-Host "I found more than 1 solution. Which one do you want to open?" -ForegroundColor Yellow
+    $solutions | Format-Table @{ Label="Solutions"; Expression={" --> $_"} }
   }
 }
 
