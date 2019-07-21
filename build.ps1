d350222361407501ebb1cc7838644c5d3f1b2a57
Nick Spreitzer <nick@200-ok.com>
2019-07-21 10:20:41 -0500

    Wrap build in try/catch

--- build.ps1
+++ build.ps1
@@ -4,56 +4,60 @@ param(
 
 $ErrorActionPreference = 'Stop'
 
-if ($env:APPVEYOR_BUILD_VERSION) {
-  $Version = $env:APPVEYOR_BUILD_VERSION
-} elseif ($Version -eq '') {
-  throw "Missing version parameter"
+try {
+  if ($env:APPVEYOR_BUILD_VERSION) {
+    $Version = $env:APPVEYOR_BUILD_VERSION
+  } elseif ($Version -eq '') {
+    throw "Missing version parameter"
+  }
+  
+  Get-ChildItem -Filter '*.nupkg' | Remove-Item 
+  Remove-Item -Path "$PSScriptRoot\publish" -Recurse -ErrorAction SilentlyContinue
+  
+  $sln = Get-ChildItem -Filter '*.sln' -Recurse | Select-Object -First 1 -ExpandProperty FullName
+  dotnet publish $sln --output .\publish -c Release
+  Get-ChildItem -Filter "WhatsNew.dll-Help.xml" -Recurse -File -Path "$PSScriptRoot\src" |
+    Where-Object { $_.FullName -like "bin\Release" } | 
+    Select-Object -First 1 | 
+    Copy-Item -Destination "$PSScriptRoot\publish" -Force
+  
+  Import-Module "$PSScriptRoot\publish\WhatsNew.dll"
+  $moduleInfo = Get-Module WhatsNew
+  $cmdletNames = Export-BinaryCmdletNames -ModuleInfo $moduleInfo
+  $cmdletAliases = Export-BinaryCmdletAliases -ModuleInfo $moduleInfo
+  
+  $scriptFiles = Get-ChildItem -Path "$PSScriptRoot\src\script-modules" -File
+  $scriptAliases = $scriptFiles | Select-Object -ExpandProperty FullName | Export-PSScriptAliases
+  $scriptFunctions = $scriptFiles | Select-Object -ExpandProperty FullName | Export-PSScriptFunctionNames
+  
+  $modules = $scriptFiles | 
+    Select-Object -ExpandProperty Name | 
+    ForEach-Object { "<APP_DATA>\script-modules\$_" }
+  
+  $modules += "<APP_DATA>\WhatsNew.dll"
+  $manifestPath = "$PSScriptRoot\publish\whats-new.psd1"
+  
+  $manifestArgs = @{
+    Path = $manifestPath
+    Guid = '861e5d28-8348-47d3-a2f6-cdd23e33bb55'
+    Author = 'Nick Spreitzer'
+    CompanyName = 'RAWR! Productions'
+    ModuleVersion = $Version
+    AliasesToExport = $cmdletAliases + $scriptAliases
+    NestedModules = $modules
+    CmdletsToExport = $cmdletNames
+    FunctionsToExport = $scriptFunctions
+  }
+  
+  New-ModuleManifest @manifestArgs
+  Import-Module "$PSScriptRoot\src\script-modules\RemoveModuleManifestComments.psm1" -Force
+  Remove-ModuleManifestComments $manifestPath -NoConfirm
+  
+  Get-ChildItem -Path "$PSScriptRoot\src" -Filter "chocolatey*.ps1" | Copy-Item -Destination .\publish
+  New-item -ItemType Directory -Path "$PSScriptRoot\publish\script-modules\" | Out-Null
+  Get-ChildItem -Path "$PSScriptRoot\src\script-modules" -Filter "*.ps*1" | Copy-Item -Destination "$PSScriptRoot\publish\script-modules\"
+  
+  choco pack "$PSScriptRoot\whats-new.nuspec" --version $Version
+} catch {
+  Write-Host $Error -ForegroundColor Red
 }
-
-Get-ChildItem -Filter '*.nupkg' | Remove-Item 
-Remove-Item -Path "$PSScriptRoot\publish" -Recurse -ErrorAction SilentlyContinue
-
-$sln = Get-ChildItem -Filter '*.sln' -Recurse | Select-Object -First 1 -ExpandProperty FullName
-dotnet publish $sln --output .\publish -c Release
-Get-ChildItem -Filter "WhatsNew.dll-Help.xml" -Recurse -File -Path "$PSScriptRoot\src" |
-  Where-Object { $_.FullName -like "bin\Release" } | 
-  Select-Object -First 1 | 
-  Copy-Item -Destination "$PSScriptRoot\publish" -Force
-
-Import-Module "$PSScriptRoot\publish\WhatsNew.dll"
-$moduleInfo = Get-Module WhatsNew
-$cmdletNames = Export-BinaryCmdletNames -ModuleInfo $moduleInfo
-$cmdletAliases = Export-BinaryCmdletAliases -ModuleInfo $moduleInfo
-
-$scriptFiles = Get-ChildItem -Path "$PSScriptRoot\src\script-modules" -File
-$scriptAliases = $scriptFiles | Select-Object -ExpandProperty FullName | Export-PSScriptAliases
-$scriptFunctions = $scriptFiles | Select-Object -ExpandProperty FullName | Export-PSScriptFunctionNames
-
-$modules = $scriptFiles | 
-  Select-Object -ExpandProperty Name | 
-  ForEach-Object { "<APP_DATA>\script-modules\$_" }
-
-$modules += "<APP_DATA>\WhatsNew.dll"
-$manifestPath = "$PSScriptRoot\publish\whats-new.psd1"
-
-$manifestArgs = @{
-  Path = $manifestPath
-  Guid = '861e5d28-8348-47d3-a2f6-cdd23e33bb55'
-  Author = 'Nick Spreitzer'
-  CompanyName = 'RAWR! Productions'
-  ModuleVersion = $Version
-  AliasesToExport = $cmdletAliases + $scriptAliases
-  NestedModules = $modules
-  CmdletsToExport = $cmdletNames
-  FunctionsToExport = $scriptFunctions
-}
-
-New-ModuleManifest @manifestArgs
-Import-Module "$PSScriptRoot\src\script-modules\RemoveModuleManifestComments.psm1" -Force
-Remove-ModuleManifestComments $manifestPath -NoConfirm
-
-Get-ChildItem -Path "$PSScriptRoot\src" -Filter "chocolatey*.ps1" | Copy-Item -Destination .\publish
-New-item -ItemType Directory -Path "$PSScriptRoot\publish\script-modules\" | Out-Null
-Get-ChildItem -Path "$PSScriptRoot\src\script-modules" -Filter "*.ps*1" | Copy-Item -Destination "$PSScriptRoot\publish\script-modules\"
-
-choco pack "$PSScriptRoot\whats-new.nuspec" --version $Version
