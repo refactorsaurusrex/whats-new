26ebf273fdf8b2ba471551ac0814615139447c1e
Nick Spreitzer <nick@200-ok.com>
2019-08-18 13:58:12 -0500

    Add cmdlet to toggle yaml front matter on and off

---
+++ src/WhatsNew/Extensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WhatsNew
+{
+    internal static class Extensions
+    {
+        public static bool YamlFrontMatterToCodeFence(this List<string> markdownLines)
+        {
+            if (markdownLines.First() != "---")
+                return false;
+
+            var counter = 0;
+            for (var i = 0; i < markdownLines.Count; i++)
+            {
+                if (markdownLines[i].StartsWith("---"))
+                {
+                    markdownLines[i] = markdownLines[i].Replace("---", "```");
+                    if (++counter >= 2)
+                        break;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool CodeFenceToYamlFrontMatter(this List<string> value)
+        {
+            if (value.First() != "```")
+                return false;
+
+            var counter = 0;
+            for (var i = 0; i < value.Count; i++)
+            {
+                if (value[i].StartsWith("```"))
+                {
+                    value[i] = value[i].Replace("```", "---");
+                    if (++counter >= 2)
+                        break;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/WhatsNew/SwitchCodeFenceToYamlFrontMatterCmdlet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using JetBrains.Annotations;
+
+namespace WhatsNew
+{
+    [PublicAPI]
+    [Cmdlet(VerbsCommon.Switch, "CodeFenceToYamlFrontMatter", ConfirmImpact = ConfirmImpact.High)]
+    public class SwitchCodeFenceToYamlFrontMatterCmdlet : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        [ValidateNotNullOrEmpty]
+        public string Path { get; set; }
+
+        [Parameter]
+        public SwitchParameter NoConfirm { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            Path = GetUnresolvedProviderPathFromPSPath(Path);
+
+            if (File.Exists(Path))
+            {
+                var warning = $"You're about to convert a code fence in the following file to yaml front matter.{Environment.NewLine}{Path}";
+                if (!NoConfirm && !ShouldContinue("Do you want to continue?", warning)) return;
+
+                var lines = File.ReadAllLines(Path).ToList();
+                if (lines.CodeFenceToYamlFrontMatter())
+                    File.WriteAllLines(Path, lines);
+            }
+            else if (Directory.Exists(Path))
+            {
+                var warning = $"You're about to convert a code fence to yaml front matter in all markdown files in the following directory.{Environment.NewLine}{Path}";
+                if (!NoConfirm && !ShouldContinue("Do you want to continue?", warning)) return;
+
+                foreach (var file in Directory.GetFiles(Path, "*.md", SearchOption.AllDirectories))
+                {
+                    var lines = File.ReadAllLines(file).ToList();
+                    if (lines.CodeFenceToYamlFrontMatter())
+                        File.WriteAllLines(file, lines);
+                }
+            }
+            else
+            {
+                throw new PSArgumentException("The specified path does not exist.");
+            }
+        }
+    }
+}
