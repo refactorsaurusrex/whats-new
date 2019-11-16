9994a2f7533c7a095b30abc34cc9eeec6ec65f35
Nick Spreitzer <nick@200-ok.com>
2019-11-16 16:38:16 -0600

    Rewrite wallpaper image download script in C#
    Entirely to improve performance via async/await calls.

---
+++ src/WhatsNew/CmdletBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace WhatsNew
+{
+    public class CmdletBase : PSCmdlet
+    {
+        private readonly Random _random = new Random();
+
+        protected void ShowProgress(ProgressRecord progressRecord, ProgressReporter progressReporter)
+        {
+            foreach (var progressInfo in progressReporter.GetConsumingEnumerable())
+            {
+                progressRecord.CurrentOperation = progressInfo.CurrentOperation;
+                progressRecord.StatusDescription = $"Completed {progressInfo.CompletedItems} of {progressInfo.TotalItems} items";
+                progressRecord.PercentComplete = progressInfo.PercentComplete();
+                WriteProgress(progressRecord);
+
+                if (!string.IsNullOrEmpty(progressInfo.VerboseOutput))
+                    WriteVerbose(progressInfo.VerboseOutput);
+            }
+        }
+
+        protected void HideProgress(ProgressRecord progressRecord)
+        {
+            progressRecord.RecordType = ProgressRecordType.Completed;
+            WriteProgress(progressRecord);
+        }
+
+        protected ProgressRecord CreateProgressRecord(string activity, string statusDescription) => new ProgressRecord(_random.Next(), activity, statusDescription);
+    }
+}
---
+++ src/WhatsNew/ProgressReporter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+
+namespace WhatsNew
+{
+    public class ProgressReporter : BlockingCollection<ProgressInfo>
+    {
+        public void UpdateProgress(string currentOperation, int totalItems, int completedItems, string verboseOutput = "")
+        {
+            var progress = new ProgressInfo(currentOperation, totalItems, completedItems, verboseOutput);
+            Add(progress);
+        }
+    }
+}
